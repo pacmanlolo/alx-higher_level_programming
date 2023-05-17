dbbc7736396cfb92c14bfdc4ba6f5e355965ae7f
pacmanlolo <michael.omotosho7@gmail.com>
2023-05-17 09:27:15 -0700

    AT LAST

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 --  lists all shows contained in hbtn_0d_tvshows without a genre linked
 -- lists all rows of a database that don't have one column
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, SUM(ratings.rating) AS rating_sum
+FROM tv_shows
+JOIN ratings ON tv_shows.id = ratings.tv_show_id
+GROUP BY tv_shows.title
+ORDER BY rating_sum DESC;
