55d7f2cb61494b88dc2a18cf10d169449e7007c4
pacmanlolo <michael.omotosho7@gmail.com>
2023-07-27 06:49:22 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
