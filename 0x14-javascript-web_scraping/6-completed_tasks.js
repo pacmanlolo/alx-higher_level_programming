4e20472968abf45426eeae9644e24c32d404977e
pacmanlolo <michael.omotosho7@gmail.com>
2023-07-27 19:04:49 -0700

    How many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completedTask = {};
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTask[todo.userId] === undefined) {
+          completedTask[todo.userId] = 1;
+        } else {
+          completedTask[todo.userId] += 1;
+        }
+      }
+    });
+    console.log(completedTask);
+  } else {
+    console.error('Error: Unable to fetch data from the API.');
+  }
+});
