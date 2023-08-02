000608e09118ae77f68ffa085321f55330c256b1
pacmanlolo <michael.omotosho7@gmail.com>
2023-08-01 17:03:20 -0700

    Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// a script that prints all characters of a Star Wars movie:
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dataCharacter = data.characters;
+  for (const x of dataCharacter) {
+    req.get(x, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
